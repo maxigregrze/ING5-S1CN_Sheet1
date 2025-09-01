6c02996515f50b3d97e618c0e14466e2490a08d5
RobinHaik <robin.haik@improba.fr>
2025-09-01 15:40:01 +0200

    Ajoutez des fichiers projet.

---
+++ Article/Program.cs
@@ -0,0 +1,36 @@
+﻿public abstract class Article
+{
+    protected string designation;
+    protected double prix;
+
+    public Article(string designation, double prix)
+    {
+        this.designation = designation;
+        this.prix = prix;
+    }
+
+    public string GetDesignation()
+    {
+        return designation;
+    }
+
+    public double GetPrix()
+    {
+        return prix;
+    }
+
+    public void SetDesignation(string designation)
+    {
+        this.designation = designation;
+    }
+
+    public void SetPrix(double prix)
+    {
+        this.prix = prix;
+    }
+
+    public void Acheter()
+    {
+        Console.WriteLine("Achat");
+    }
+}
